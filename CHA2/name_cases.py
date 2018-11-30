bccf498dea01f6ac1d0da6ae574dabcfe597b373
JayF4n <jayf4n@gmail.com>
2018-12-01 00:08:05 +0800

    Classified by chapter

---
+++ CHA2/name_cases.py
@@ -0,0 +1,21 @@
+name = "Eric"
+
+print("Hello " + name + ", would you like to learn some Pyhon today?")
+
+name = "eric topson"
+
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+
+print('鲁迅曾经说过，“罗马不是一天建成的。"')
+
+famous_person = "鲁迅"
+message = "罗马不是一天建成的。"
+print(famous_person + '曾经说过，' + '\n\t"罗马不是一天建成的。”')
+
+full_name = " johny brown "
+print(full_name.lstrip())
+print(full_name.rstrip())
+print(full_name.strip())
---
+++ CHA2/number.py
@@ -0,0 +1,10 @@
+age = 23
+
+print("Happy " + str(age) + "rd Birthday!")
+
+
+print(type(age))
+
+favarite_num = 0
+
+print("My favarite number is " + str(favarite_num) + "!")
