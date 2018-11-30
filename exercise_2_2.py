3777ce0a8e4afef10c3a5574459d2fa832dec6c2
JayF4n <jayf4n@gmail.com>
2018-11-30 21:41:13 +0800

    commit

--- exercise_2_2.py
+++ exercise_2_2.py
@@ -13,3 +13,14 @@ name = "Ada LovelAce"
 
 print(name.upper())
 print(name.lower())
+
+first_name = "ada"
+
+last_name = "lovelace"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+print("Hello," + full_name.title())
+print("Languge:\nPython\nC\nJavaScript")
+print("Language:\n\tPython\n\tC\n\tJavaScript")
---
+++ name_cases.py
@@ -0,0 +1,16 @@
+name = "Eric"
+
+print("Hello " + name + ", would you like to learn some Pyhon today?")
+
+name = "eric topson"
+
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+
+print('鲁迅曾经说过，“罗马不是一天建成的。"')
+
+famous_person = "鲁迅"
+message = "罗马不是一天建成的。"
+print(famous_person + '曾经说过，' + '\n\t"罗马不是一天建成的。”')
