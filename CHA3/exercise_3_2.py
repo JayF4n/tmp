a7e734ee3baa2a1756490ee47f5c1702b29e0860
JayF4n <jayf4n@gmail.com>
2018-12-01 14:43:09 +0800

    commit

---
+++ CHA3/exercise_3_2.py
@@ -0,0 +1,13 @@
+name = ['john','sue','glumo']
+
+print(name[0].title())
+print(name[1].title())
+print(name[2].title())
+
+print(name[-3].title() + ' likes to play DOTA2')
+
+print(name[-2].title() + ' likes to play Artifact')
+
+print(name[-1].title() + ' likes to play CSGO')
+
+
